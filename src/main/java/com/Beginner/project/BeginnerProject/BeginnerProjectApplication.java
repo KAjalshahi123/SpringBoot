c189037cad0d0e6adff859a5acd7fa4da9479193
Shahikajal <shahikajal274508@gmail.com>
2025-05-08 00:07:04 +0530

    Using curd opertion

---
+++ src/main/java/com/Beginner/project/BeginnerProject/BeginnerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Beginner.project.BeginnerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginnerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeginnerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Beginner/project/BeginnerProject/BeginnerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Beginner.project.BeginnerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
